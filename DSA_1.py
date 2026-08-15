02b952831e07d4dba6d2210f4511b95cd80ca8e9
manoj singh <manojsingh76543212@gmail.com>
2026-08-15 21:33:06 +0530

    Implement majority element function to find the element that appears more than half the time in a list

--- DSA_1.py
+++ DSA_1.py
@@ -1,1 +1,16 @@
 nums = [2,2,1,1,1,2,2]
+
+def majorityElement(nums):
+    count = {}
+    for num in nums:
+        if num in count:
+            count[num] += 1
+        else:
+            count[num] = 1
+            
+    majority_count = len(nums) // 2
+    for num, cnt in count.items():
+        if cnt > majority_count:
+            return num
+
+print(majorityElement(nums))
